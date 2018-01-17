401ceab83fe91f7c7ee515e1df9caa88e9dd1fd9
Christiaan Knaap <christiaanfurore@outlook.com>
2018-01-17 10:00:37 +0100

    devops: Update certificate script

--- scripts/GenerateSSLCertificate.ps1
+++ scripts/GenerateSSLCertificate.ps1
@@ -1,9 +1,42 @@
-﻿
+﻿$certFileName = "ssl_cert"
+$certPassword = "cert-password"
 $certStore = "cert:\CurrentUser\My"
-$cert = New-SelfSignedCertificate -DnsName "localhost" -CertStoreLocation $certStore
-Write-Host $cert
-$PFXPass = ConvertTo-SecureString -String “cert-password” -Force -AsPlainText
-$certLocation = $certStore + "\" + $cert.Thumbprint
-Write-Host $certLocation
-$exportDir = $PSScriptRoot + "\..\Vonk.IdentityServer.Test\ssl_cert.pfx"
-Export-PfxCertificate -Cert $certLocation -Password $PFXPass -FilePath "c:\git\Vonk.IdentityServer.Test\Vonk.IdentityServer.Test\ssl_cert.pfx"
+$site = "localhost"
+
+# setup certificate properties including the commonName (DNSName) property
+$certificate = New-SelfSignedCertificate `
+    -Subject $site `
+    -DnsName $site `
+    -KeyAlgorithm RSA `
+    -KeyLength 2048 `
+    -NotBefore (Get-Date) `
+    -NotAfter (Get-Date).AddYears(2) `
+    -CertStoreLocation $certStore `
+    -FriendlyName "Localhost Certificate for .NET Core" `
+    -HashAlgorithm SHA256
+
+$certificatePath = $certStore + "\" + ($certificate.ThumbPrint)  
+
+# create temporary certificate path
+$exportDir = $PSScriptRoot + "\..\Vonk.IdentityServer.Test"
+
+# set certificate password here
+$pfxPassword = ConvertTo-SecureString -String $certPassword -Force -AsPlainText
+$pfxFilePath = "$exportDir\$certFileName.pfx"
+$cerFilePath = "$exportDir\$certFileName.cer"
+
+Write-Host "cerFilePath = $cerFilePath"
+
+# create pfx certificate
+Export-PfxCertificate -Cert $certificatePath -FilePath $pfxFilePath -Password $pfxPassword
+Export-Certificate -Cert $certificatePath -FilePath $cerFilePath
+
+# import the pfx certificate
+Import-PfxCertificate -FilePath $pfxFilePath Cert:\LocalMachine\My -Password $pfxPassword -Exportable
+
+# trust the certificate by importing the pfx certificate into your trusted root
+Import-Certificate -FilePath $cerFilePath -CertStoreLocation Cert:\CurrentUser\Root
+
+# optionally delete the physical certificates (don’t delete the pfx file as you need to copy this to your app directory)
+# Remove-Item $pfxFilePath
+Remove-Item $cerFilePath
