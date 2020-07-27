71cff8dd96f6ab4617af85dda2a65fcb916c812c
Marco Visser <marco@fire.ly>
2020-07-27 12:27:28 +0200

    fix: swicth off localhost

--- Vonk.IdentityServer.Test/Program.cs
+++ Vonk.IdentityServer.Test/Program.cs
@@ -14,14 +14,18 @@ namespace Vonk.IdentityServer
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
-                .UseKestrel(options =>
+                .UseKestrel(
+#if DEBUG          
+            options =>
                 {
                     options.Listen(IPAddress.Loopback, 5100);
                     options.Listen(IPAddress.Loopback, 5101, listenOptions =>
                     {
                         listenOptions.UseHttps("ssl_cert.pfx", "cert-password");
                     });
-                })
+                }
+#endif
+                )
             .Build();
     }
 }
