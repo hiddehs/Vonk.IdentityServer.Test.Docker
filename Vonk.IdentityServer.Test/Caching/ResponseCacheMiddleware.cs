0e00a0953eb340cff21bfae2fda5b4b4acbc650a
Alexander Zautke <alexander@fire.ly>
2020-08-10 16:47:20 +0200

    Add logging to ResponseCacheMiddleware

--- Vonk.IdentityServer.Test/Caching/ResponseCacheMiddleware.cs
+++ Vonk.IdentityServer.Test/Caching/ResponseCacheMiddleware.cs
@@ -1,33 +1,36 @@
 ﻿using System;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Vonk.IdentityServer.Test.Support;
 
 namespace Vonk.IdentityServer
 {
     internal class ResponseCacheMiddleware
     {
         private readonly RequestDelegate _next;
+        private readonly ILogger<ResponseCacheMiddleware> _logger;
 
-        public ResponseCacheMiddleware(RequestDelegate next)
+        public ResponseCacheMiddleware(RequestDelegate next, ILogger<ResponseCacheMiddleware> logger)
         {
-            if (next == null)
-                throw new ArgumentNullException(nameof(next));
-
+            Check.NotNull(next, nameof(next));
+            Check.NotNull(logger, nameof(logger));
             _next = next;
+            _logger = logger;
         }
 
         public async Task Invoke(HttpContext httpContext)
         {
-            if (httpContext == null)
-                throw new ArgumentNullException(nameof(httpContext));
+            Check.NotNull(httpContext, nameof(httpContext));
 
             httpContext.Response.GetTypedHeaders().CacheControl =
             new Microsoft.Net.Http.Headers.CacheControlHeaderValue()
             {
                 NoStore = true
             };
-
             httpContext.Response.Headers.Add("pragma", "no-cache");
+
+            _logger.LogDebug("ResponseCacheMiddleware - Added CacheControl and Pragma header");
 
             await _next(httpContext);
         }
